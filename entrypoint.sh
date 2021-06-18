ddd06114a89ad590333a015b4ccadd7188ea718c
Aliaksandr <minchik@gmail.com>
2021-06-18 22:41:33 +0300

    Cleanup .ssh folder after sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 # Do deployment
 sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${INPUT_RSYNC_SOURCE} ${SSH_USERNAME}@${SSH_HOSTNAME}:${INPUT_RSYNC_TARGET}"
+rm -rf $SSH_PATH
